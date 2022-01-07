7adfa890aabfea80618d91051667090ec9101774
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-01-07 22:05:40 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+Console.WriteLine("Hello, World!");
+
+//Create a class to hold new employee information.
+
+public class Employee
+{
+    [Key]
+    public long Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Title { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string? Department { get; set; }
+    public string? Manager { get; set; }
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Zip { get; set; }
+    public string? Country { get; set; }
+    public string? Notes { get; set; }
+}
+
+
